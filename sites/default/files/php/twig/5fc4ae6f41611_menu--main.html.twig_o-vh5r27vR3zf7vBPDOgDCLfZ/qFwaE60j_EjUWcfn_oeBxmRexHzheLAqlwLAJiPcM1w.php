7c38fd17e66cc979f50d77a8145290de2909c89e
ajawadmirza <Abdul.Razzaque@visionetsystems.com>
2020-11-30 13:38:59 +0500

    frontpage changes

---
+++ sites/default/files/php/twig/5fc4ae6f41611_menu--main.html.twig_o-vh5r27vR3zf7vBPDOgDCLfZ/qFwaE60j_EjUWcfn_oeBxmRexHzheLAqlwLAJiPcM1w.php
@@ -0,0 +1,221 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/etisalat/templates/home/menu--main.html.twig */
+class __TwigTemplate_e7e94a115252397e88b66e6e7cdc994ea9d3894018d68556d84f05d4239f052c extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["import" => 21, "macro" => 29, "if" => 31, "for" => 34, "set" => 36];
+        $filters = ["escape" => 43];
+        $functions = ["link" => 44];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['import', 'macro', 'if', 'for', 'set'],
+                ['escape'],
+                ['link']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 21
+        $context["menus"] = $this;
+        // line 22
+        echo "
+";
+        // line 27
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($context["menus"]->getmenu_links(($context["items"] ?? null), ($context["attributes"] ?? null), 0));
+        echo "
+
+";
+    }
+
+    // line 29
+    public function getmenu_links($__items__ = null, $__attributes__ = null, $__menu_level__ = null, ...$__varargs__)
+    {
+        $context = $this->env->mergeGlobals([
+            "items" => $__items__,
+            "attributes" => $__attributes__,
+            "menu_level" => $__menu_level__,
+            "varargs" => $__varargs__,
+        ]);
+
+        $blocks = [];
+
+        ob_start();
+        try {
+            // line 30
+            echo "  ";
+            $context["menus"] = $this;
+            // line 31
+            echo "  ";
+            if (($context["items"] ?? null)) {
+                // line 32
+                echo "    <div class=\"collapse navbar-collapse\" id=\"etisalat-navbar\">
+        <ul class=\"navbar-nav mr-auto\">
+    ";
+                // line 34
+                $context['_parent'] = $context;
+                $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+                foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+                    // line 35
+                    echo "      ";
+                    // line 36
+                    $context["classes"] = [0 => "menu-item", 1 => (($this->getAttribute(                    // line 38
+$context["item"], "is_expanded", [])) ? ("menu-item--expanded") : ("")), 2 => (($this->getAttribute(                    // line 39
+$context["item"], "is_collapsed", [])) ? ("menu-item--collapsed") : ("")), 3 => (($this->getAttribute(                    // line 40
+$context["item"], "in_active_trail", [])) ? ("menu-item--active-trail") : (""))];
+                    // line 43
+                    echo "      <li";
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute($this->getAttribute($context["item"], "attributes", []), "addClass", [0 => ($context["classes"] ?? null), 1 => "nav-item nav-link"], "method")), "html", null, true);
+                    echo ">
+        ";
+                    // line 44
+                    echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->getLink($this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "title", [])), $this->sandbox->ensureToStringAllowed($this->getAttribute($context["item"], "url", []))), "html", null, true);
+                    echo "
+        ";
+                    // line 45
+                    if ($this->getAttribute($context["item"], "below", [])) {
+                        // line 46
+                        echo "          ";
+                        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar($context["menus"]->getmenu_links($this->getAttribute($context["item"], "below", []), ($context["attributes"] ?? null), (($context["menu_level"] ?? null) + 1)));
+                        echo "
+        ";
+                    }
+                    // line 48
+                    echo "      </li>
+    ";
+                }
+                $_parent = $context['_parent'];
+                unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+                $context = array_intersect_key($context, $_parent) + $_parent;
+                // line 50
+                echo "  </ul>
+  ";
+            }
+        } catch (\Exception $e) {
+            ob_end_clean();
+
+            throw $e;
+        } catch (\Throwable $e) {
+            ob_end_clean();
+
+            throw $e;
+        }
+
+        return ('' === $tmp = ob_get_clean()) ? '' : new Markup($tmp, $this->env->getCharset());
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/etisalat/templates/home/menu--main.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  126 => 50,  119 => 48,  113 => 46,  111 => 45,  107 => 44,  102 => 43,  100 => 40,  99 => 39,  98 => 38,  97 => 36,  95 => 35,  91 => 34,  87 => 32,  84 => 31,  81 => 30,  67 => 29,  60 => 27,  57 => 22,  55 => 21,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{#
+/**
+ * @file
+ * Theme override to display a menu.
+ *
+ * Available variables:
+ * - menu_name: The machine name of the menu.
+ * - items: A nested list of menu items. Each menu item contains:
+ *   - attributes: HTML attributes for the menu item.
+ *   - below: The menu item child items.
+ *   - title: The menu link title.
+ *   - url: The menu link url, instance of \\Drupal\\Core\\Url
+ *   - localized_options: Menu link localized options.
+ *   - is_expanded: TRUE if the link has visible children within the current
+ *     menu tree.
+ *   - is_collapsed: TRUE if the link has children within the current menu tree
+ *     that are not currently visible.
+ *   - in_active_trail: TRUE if the link is in the active trail.
+ */
+#}
+{% import _self as menus %}
+
+{#
+  We call a macro which calls itself to render the full tree.
+  @see https://twig.symfony.com/doc/1.x/tags/macro.html
+#}
+{{ menus.menu_links(items, attributes, 0) }}
+
+{% macro menu_links(items, attributes, menu_level) %}
+  {% import _self as menus %}
+  {% if items %}
+    <div class=\"collapse navbar-collapse\" id=\"etisalat-navbar\">
+        <ul class=\"navbar-nav mr-auto\">
+    {% for item in items %}
+      {%
+        set classes = [
+          'menu-item',
+          item.is_expanded ? 'menu-item--expanded',
+          item.is_collapsed ? 'menu-item--collapsed',
+          item.in_active_trail ? 'menu-item--active-trail',
+        ]
+      %}
+      <li{{ item.attributes.addClass(classes, 'nav-item nav-link') }}>
+        {{ link(item.title, item.url) }}
+        {% if item.below %}
+          {{ menus.menu_links(item.below, attributes, menu_level + 1) }}
+        {% endif %}
+      </li>
+    {% endfor %}
+  </ul>
+  {% endif %}
+{% endmacro %}
+", "themes/etisalat/templates/home/menu--main.html.twig", "C:\\xampp\\htdocs\\drupal\\theming-etisalat\\themes\\etisalat\\templates\\home\\menu--main.html.twig");
+    }
+}
